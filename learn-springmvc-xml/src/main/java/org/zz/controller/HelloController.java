a9affb20bdc2a4a16b55eb19809df6c3c2f0b3e1
xulei131401 <373045134@qq.com>
2024-07-11 19:00:59 +0800

    fix

---
+++ learn-springmvc-xml/src/main/java/org/zz/controller/HelloController.java
@@ -0,0 +1,24 @@
+package org.zz.controller;
+
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+
+//public class HelloController implements Controller {
+public class HelloController {
+//    @Override
+//    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//        ModelAndView mv = new ModelAndView();
+//        mv.addObject("msg", "helloSpringMvc");
+//        mv.setViewName("hello");
+//        return mv;
+//    }
+//
+//    @Override
+//    public ModelAndView handleRequest(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws Exception {
+//        return null;
+//    }
+}
---
+++ learn-springmvc-xml/src/main/java/org/zz/controller/HelloController2.java
@@ -0,0 +1,14 @@
+package org.zz.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController2 {
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("msg", "hello");
+        return "hello";
+    }
+}
